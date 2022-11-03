28c515fc48ded316909b3957f054fcb891058bb3
Chinmay Bag <chinmay2893@gmail.com>
2022-11-03 17:14:16 +0530

    Replace default App.tsx contents with code to connect with Metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as ethers from 'ethers';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [haveMetamask, setHaveMetamask] = useState(false);
+  const [isConnected, setIsConnected] = useState(false);
+  const [accountAddress, setAccountAddress] = useState('');
+  const [accountBalance, setAccountBalance] = useState('');
+
+  const checkMetamaskAvailability = () => {
+    // @ts-ignore-next-line
+    if (!window.ethereum) {
+      setHaveMetamask(false);
+    }
+    setHaveMetamask(true);
+  };
+
+  const getWalletAddress = (address: string) => {
+    if (!address) return '';
+    return `${address.slice(0, 8)}....${address.slice(34, 42)}`;
+  };
+
+  const connectWallet = async () => {
+    try {
+
+      // @ts-ignore-next-line
+      if (!window.ethereum) {
+        setHaveMetamask(false);
+      }
+
+      // @ts-ignore-next-line
+      const accounts: string[] = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      const walletAddress = getWalletAddress(accounts[0])
+      setAccountAddress(walletAddress);
+      setIsConnected(true);
+
+      // @ts-ignore-next-line
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const balance = await provider.getBalance(accounts[0]);
+      const bal = ethers.utils.formatEther(balance);
+      setAccountBalance(bal);
+
+    } catch (err) {
+      setIsConnected(false);
+    }
+  };
+
+  useEffect(() => {
+    checkMetamaskAvailability();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{color: 'gray' }}>
+          <h2>Crypto balance checker</h2>
+        </div>
+        {
+          haveMetamask ? (
+            <div>
+              <button onClick={connectWallet} disabled={isConnected} >Connect wallet</button>
+              {(isConnected && accountAddress && accountBalance) && (
+                <p>
+                  <b>Address: </b>{accountAddress}
+                  <br></br>
+                  <b>Balance: </b>{(+accountBalance).toFixed(6)} ETH
+                </p>
+              )}
+            </div>
+          ) : (
+            <div><h3>Please install metamask</h3></div>
+          )
+        }
       </header>
     </div>
   );
